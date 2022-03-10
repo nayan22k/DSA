adcf832c8ec42e20275dad31a29b89043132ba53
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-11 00:46:16 +0530

    Add files via upload

---
+++ countDigits.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class countDigits {
+    static int countDigit(long n)
+    {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            ++count;
+        }
+        return count;
+    }
+
+    /* Driver code */
+    public static void main(String[] args)
+    {
+        long n = 345289467;
+        System.out.print("Number of digits : "
+                + countDigit(n));
+    }
+
+}
